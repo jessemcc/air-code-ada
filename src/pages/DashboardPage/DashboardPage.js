c34b063e6cdc274dcfe67d3df3174a7b6eb437d0
Dylan LaRocque <dylan@Dylans-MacBook-Pro-2.local>
2023-02-03 08:52:04 -0700

    initial setup

--- src/pages/DashboardPage/DashboardPage.js
+++ src/pages/DashboardPage/DashboardPage.js
@@ -1,5 +1,22 @@
+import './DashboardPage.scss'
+
 const DashboardPage = () => {
-  return <></>;
+  return (
+  <>
+  <section className="shop-container">
+    <article className="partners">
+      <h2 className="partners__title">Featured Partners</h2>
+    </article>
+    <article className="categories">
+      <h2 className="categories__title">Top Categories</h2>
+    </article>
+    <article className="products">
+      <h2 className="products__title">Top Products</h2>
+    </article>
+
+  </section>
+  </>
+  );
 };
 
 export default DashboardPage;
