be06dd4c4a0356e1a1b28a2f2962ab5de67f0d40
jessemcc <jesse.mcconnell@live.ca>
2023-02-03 13:57:36 -0500

    push to pull

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,41 +7,47 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <section className="header">
-      <div className="header__logo">
-        <img src={aeroplan} alt="aeroplan logo" className="header__aeroplan" />
-        <img
-          src={airCanada}
-          alt="air canada logo"
-          className="header__air-canada"
-        />
-      </div>
-      <div className="header__nav-container">
-        <ul className="header__nav-list">
-          <Link to="">
-            <li className="heaver__nav-item">Home</li>
-          </Link>
-          <Link to="">
-            <li className="heaver__nav-item">Book</li>
-          </Link>
-          <Link to="">
-            <li className="heaver__nav-item">Plan</li>
-          </Link>
-          <Link to="">
-            <li className="heaver__nav-item">Fly</li>
-          </Link>
-          <Link to="">
-            <li className="heaver__nav-item">Benefits</li>
-          </Link>
-        </ul>
-        <div className="header__profile">
+      <article className="header__container">
+        <div className="header__logo">
           <img
-            src={profilePic}
-            alt="Profile Picture"
-            className="header__profile-image"
+            src={aeroplan}
+            alt="aeroplan logo"
+            className="header__aeroplan"
           />
-          <p className="header__name">Rachel 235pts</p>
+          <img
+            src={airCanada}
+            alt="air canada logo"
+            className="header__air-canada"
+          />
         </div>
-      </div>
+        <div className="header__nav-container">
+          <ul className="header__nav-list">
+            <Link className="header__link" to="">
+              <li className="header__nav-item">Home</li>
+            </Link>
+            <Link className="header__link" to="">
+              <li className="header__nav-item">Book</li>
+            </Link>
+            <Link className="header__link" to="">
+              <li className="header__nav-item">Plan</li>
+            </Link>
+            <Link className="header__link" to="">
+              <li className="header__nav-item">Fly</li>
+            </Link>
+            <Link className="header__link" to="">
+              <li className="header__nav-item">Benefits</li>
+            </Link>
+          </ul>
+          <div className="header__profile">
+            <img
+              src={profilePic}
+              alt="Profile Picture"
+              className="header__profile-image"
+            />
+            <p className="header__name">Rachel 235pts</p>
+          </div>
+        </div>
+      </article>
     </section>
   );
 };
