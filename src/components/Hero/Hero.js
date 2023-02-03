f85afbb2bd6843599746611293811a08bbfbea77
jessemcc <jesse.mcconnell@live.ca>
2023-02-03 13:23:52 -0500

    added hello name

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,7 @@
+import hero from "../../assets/Images/hero-img.png";
+
+const Hero = () => {
+  return <img className="hero-image" alt="image of hero section" src={hero} />;
+};
+
+export default Hero;
