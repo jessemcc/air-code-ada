6e5051d59d2c26e0972bc091f7eb2bdf52c78361
Kimberlynolsin <kimberly.nolsin@gmail.com>
2023-02-03 12:00:10 -0500

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import footer from "../../assets/Images/footer.png";
+import './Footer.scss'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <img className="footer__img" src={footer}></img>
+    </footer>
+  );
+};
+
+export default Footer;
