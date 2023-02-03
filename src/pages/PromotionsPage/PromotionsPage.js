1cc474eec41943ff86a69f7a447eac8aa7d12b3a
Kimberlynolsin <kimberly.nolsin@gmail.com>
2023-02-03 11:43:37 -0500

    Finalized styling

--- src/pages/PromotionsPage/PromotionsPage.js
+++ src/pages/PromotionsPage/PromotionsPage.js
@@ -5,7 +5,7 @@ import lcbo2 from "../../assets/Images/offers-lcbo-06.png";
 import starbucks1 from "../../assets/Images/offers-starbucks-02.png";
 import starbucks2 from "../../assets/Images/offers-starbucks-05.png";
 import uber from "../../assets/Images/offers-uber-07-en.png";
-import './PromotionsPage.scss'
+import "./PromotionsPage.scss";
 
 const PromotionsPage = () => {
   const cardArr = [
@@ -44,10 +44,12 @@ const PromotionsPage = () => {
   const displayCard = cardArr.map((element) => {
     return (
       <div className="card__container" key={element.id}>
-        <img className="card__img" src={element.image} alt='cards'></img>
+        <img className="card__img" src={element.image} alt="cards"></img>
         <div className="card__details">
           <h4 className="card__subtitle">{element.subtitle}</h4>
-          <Link to='#'><p className="card__unlock">Unlock offer</p></Link>
+          <Link to="#" className="card__unlock">
+            <p className="card__unlock__p">Unlock offer</p>
+          </Link>
         </div>
       </div>
     );
@@ -55,7 +57,12 @@ const PromotionsPage = () => {
 
   return (
     <section className="card">
-      <h1 className="card__title"> Your Exclusive Offers</h1>
+      <div className="card__box">
+        <h1 className="card__title"> Your Exclusive Offers</h1>
+        <Link to="#" className="card__link">
+          <p className="card__link__text">See All</p>
+        </Link>
+      </div>
       <div className="card__wrapper">{displayCard}</div>
     </section>
   );
